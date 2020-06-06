15f4f661278bb943a26674106f664bb3dc8c5474
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-06 11:33:04 +0900

    XML / 코틀린을 연결해서 코틀린 클래스에서 동작 코드

--- app/src/main/java/kr/ptus/fragment_20200606/fragment/FirstFragment.kt
+++ app/src/main/java/kr/ptus/fragment_20200606/fragment/FirstFragment.kt
@@ -1,9 +1,30 @@
 package kr.ptus.fragment_20200606.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
+import kr.ptus.fragment_20200606.R
 
 class FirstFragment : Fragment() {
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+       return inflater.inflate(R.layout.fragment_first,container,false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        messageTxt.text = "안녕하세요."
+
+    }
 
 
 }
