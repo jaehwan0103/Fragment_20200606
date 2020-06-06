c85e1b72b05c7a405d053468db887b5a1b4a9ba0
jaehwan0103 <dkdlfkqb1@gmail.com>
2020-06-06 12:33:17 +0900

    뷰페이저 액티비티 추가 => 앱의 시작점으로 활용

---
+++ app/src/main/java/kr/ptus/fragment_20200606/ViewPagerActivity.kt
@@ -0,0 +1,30 @@
+package kr.ptus.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPagerActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    override fun setupEvents() {
+
+
+
+    }
+
+    override fun setValues() {
+
+
+
+    }
+
+}
